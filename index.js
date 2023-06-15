90a402e92d5bd66b1c09e4ee418c2941dfbd7e10
Imran-A-K <imran.k.professional@gmail.com>
2023-06-15 19:48:33 +0600

    added jwt verifying function

--- index.js
+++ index.js
@@ -15,70 +15,88 @@ const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
+}
+app.use(cors(corsOptions))
 // handing json conversion
-  app.use(express.json())
+app.use(express.json())
 
 
+// jwt verifying function
+const validateJWT = async (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'You are not authorized' })
+    }
+    const token = authorization.split(' ')[1];
   
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5j7d2x6.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (error, decoded) => {
+      if (error) {
+        return res.status(401).send({ error: true, message: 'You are not authorized' })
+      }
+  
+      req.decoded = decoded;
+      next()
+    })
   }
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5j7d2x6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+
 async function run() {
-  try {
-    
-    
-    const productCollection = client.db('expressBuy').collection('products');
+    try {
 
-    // get all the products
-    app.get('/products', async(req, res) => {
-        const page = parseInt(req.query.page) || 0;
-        const limit = parseInt(req.query.limit) || 10;
-        const skip = page * limit
-          const result = await productCollection.find().skip(skip).limit(limit).toArray();
-          res.send(result);
-      })
-  
-      // get total products count
-      app.get('/totalProducts', async(req,res) => {
-          const result = await productCollection.estimatedDocumentCount();
-          res.send({ totalProducts: result });
-      })
-      
-      // jwt token sender api
 
-    app.post('/jwt', (req, res) => {
-        const requester = req.body;
-  
-        const token = jwt.sign(requester, process.env.ACCESS_TOKEN_SECRET_KEY, {
-          expiresIn: '1h'
+        const productCollection = client.db('expressBuy').collection('products');
+
+        // get all the products
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page) || 0;
+            const limit = parseInt(req.query.limit) || 10;
+            const skip = page * limit
+            const result = await productCollection.find().skip(skip).limit(limit).toArray();
+            res.send(result);
         })
-       
-        res.send({ token });
-      })
-  
+
+        // get total products count
+        app.get('/totalProducts', async (req, res) => {
+            const result = await productCollection.estimatedDocumentCount();
+            res.send({ totalProducts: result });
+        })
+
+        // jwt token sender api
+
+        app.post('/jwt', (req, res) => {
+            const requester = req.body;
+
+            const token = jwt.sign(requester, process.env.ACCESS_TOKEN_SECRET_KEY, {
+                expiresIn: '1h'
+            })
+
+            res.send({ token });
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    
-  }
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
 }
 run().catch(console.dir);
 
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Express Buy Server is operating')
-  })
-  app.listen(port, () => {
+})
+app.listen(port, () => {
     console.log(`Express Buy Server is operating on port ${port}`)
-  })
+})
